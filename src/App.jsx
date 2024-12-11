963a8f122062e56c5a7bd24c83042889fd45a839
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-10 23:02:05 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import data from "./DataBase/Data";
 import Home from "./Components/Home/Home";
-import Product from "./Components/Product/Product";
 import Cart from "./Components/Cart/Cart";
+import Login from "./Components/Login/Login";
+import Product from "./Components/Product/Product";
+import Profile from "./Components/Profile/Profile";
 import ProductInfo from "./Components/ProductInfo/ProductInfo";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import data from "./DataBase/Data";
-import Login from "./Components/Login/Login";
-import Profile from "./Components/Profile/Profile";
 
 function App() {
   return (
@@ -15,9 +15,8 @@ function App() {
         <Route exact path="/Home" element={<Home />} />
         <Route exact path="/Product" element={<Product />} />
         <Route exact path="/Product-Info/:productId" element={<ProductInfo data={data}/>} />
-        <Route exact path="/Cart" element={<Cart />} />
         <Route exact path="/Profile" element={<Profile />} />
-
+        <Route exact path="/Cart" element={<Cart />} />
       </Routes>
     </Router>
   );
